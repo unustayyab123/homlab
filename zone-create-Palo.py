61a48fe4ae2e19cadce63258c9462317b20c66f8
younus <unustayyab@hotmail.com>
2023-03-13 20:43:13 +0300

    new Commit

--- zone-create-Palo.py
+++ zone-create-Palo.py
@@ -1,3 +1,5 @@
+# ******Create New Zone on Malqa DC-FW****
+
 from netmiko import ConnectHandler
 Device = {
         "device_type": "paloalto_panos",
@@ -6,13 +8,23 @@ Device = {
         'password': "admin123",
 }
 ssh123 = ConnectHandler(**Device)
+##ZONE NAMES
+Zone_list = open(r"E:\Network Automation\vlans-name.txt",)
+Zone_Name = Zone_list.read().splitlines()
 
-# ******Create New Zone on Malqa DC-FW****
+###INTERFACE NUMBER FOR ZONE
+INTF_NUMBER = open(r"E:\Network Automation\Zone-Interfaces.txt",)
+intf_num = INTF_NUMBER.read().splitlines()
 
-VLAN_list = open(r"E:\Network Automation\vlans-name.txt",)
-vlan_Name = VLAN_list.read().splitlines()
+intf_count = print(len(intf_num))
 
-for name in vlan_Name:
-        Zone_create = ssh123.send_config_set("set zone "+name + " network layer3" )
-        print(Zone_create)
-        print( "Zone" + name + "created ")
+###CREATING ZONES
+r=0
+while r<len(intf_num):
+ for name in Zone_Name:
+             ssh123 = ConnectHandler(**Device)
+             # cmds123 = ["vlan " + number, "name " + vlan_Name[0+r]]
+             Zone_create = ssh123.send_config_set("set zone " + name + " network layer3 " +intf_num[0+r])
+             print(Zone_create)
+             print("Zone  " + name + "  created Successfully")
+             r=r+1
